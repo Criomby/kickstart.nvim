1c246e17284c08800288120ff311d75cccafb8af
Philippe <criomby@protonmail.com>
2026-05-02 17:50:43 +0200

    feat: indent width & ftplugin

---
+++ ftplugin/sh.lua
@@ -0,0 +1,11 @@
+-- Set tab stop
+vim.opt_local.tabstop = 2
+
+-- Set soft tab stop (used for auto-indenting)
+vim.opt_local.softtabstop = 2
+
+-- Set shift width (used for indent/unindent commands)
+vim.opt_local.shiftwidth = 2
+
+-- Use spaces instead of tabs for indentation
+vim.opt_local.expandtab = true
